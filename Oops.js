5578a990caca76541f221ffa8a7d16934fc62123
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2021-02-03 04:17:34 -1000

    Understand where an object's prototype comes from

--- Oops.js
+++ Oops.js
@@ -151,4 +151,11 @@ Dog.prototype = {
     }
 };
 
-//
+// Understand where an object's prototype comes from
+function Dog(name) {
+    this.name = name;
+}
+
+let beagle = new Dog("Snoopy");
+
+Dog.prototype.isPrototypeOf(beagle);
