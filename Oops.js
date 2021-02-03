dabeaea0cd6cf95521f6e1289a101828139cc9ed
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2021-02-03 04:34:35 -1000

    Inherit behaviors from a supertype

--- Oops.js
+++ Oops.js
@@ -196,4 +196,17 @@ Animal.prototype = {
     }
 };
 
-//
+// Inherit behaviors from a supertype
+function Animal() { }
+
+Animal.prototype = {
+  constructor: Animal,
+  eat: function() {
+    console.log("nom nom nom");
+  }
+};
+
+let duck = Object.create(Animal.prototype);
+let beagle = Object.create(Animal.prototype); 
+
+// 
