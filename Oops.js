ede4c9231d28bed98c6018d3b3639cca12d4a952
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2021-02-03 04:40:43 -1000

    reset an inherited construction property

--- Oops.js
+++ Oops.js
@@ -223,4 +223,18 @@ function Dog() { }
 Dog.prototype = Object.create(Animal.prototype);
 let beagle = new Dog();
 
+// Reset an inherited constructor property
+function Animal() { }
+function Bird() { }
+function Dog() { }
+
+Bird.prototype = Object.create(Animal.prototype);
+Dog.prototype = Object.create(Animal.prototype);
+
+Bird.prototype.constructor = Bird;
+Dog.prototype.constructor = Dog;
+
+let duck = new Bird();
+let beagle = new Dog();
+
 // 
