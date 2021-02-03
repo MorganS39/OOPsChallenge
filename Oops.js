ac39430e96e9df877e2252c9e9990db8c0586820
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2021-02-03 04:04:39 -1000

    Understand the constructor property

--- Oops.js
+++ Oops.js
@@ -107,4 +107,17 @@ for (let property in beagle) {
 console.log(ownProps);
 console.log(prototypeProps);
 
+// Understand the constructor property
+function Dog(name) {
+    this.name = name;
+}
+  
+function joinDogFraternity(candidate) {
+    if (candidate.constructor === Dog) {
+      return true;
+    } else {
+      return false;
+    }
+}
+
 //
