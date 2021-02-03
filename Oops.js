a0369110c2cbc335a15f7637e2f09fc57ce6d67a
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2021-02-03 04:47:57 -1000

    Override inherited methods

--- Oops.js
+++ Oops.js
@@ -251,4 +251,20 @@ Dog.prototype.bark = function() {
 
 let beagle = new Dog();
 
+// Override inherited methods
+function Bird() { }
+
+Bird.prototype.fly = function() { return "I am flying!"; };
+
+function Penguin() { }
+Penguin.prototype = Object.create(Bird.prototype);
+Penguin.prototype.constructor = Penguin;
+
+Penguin.prototype.fly = function() {
+    return "Alas, this is a flightless bird.";
+};
+
+let penguin = new Penguin();
+console.log(penguin.fly());
+
 //
