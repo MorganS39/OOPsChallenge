70f0b2a860c5bf7cbc8438c2734422080ebd5d84
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2021-02-03 04:14:42 -1000

    set constructor property when changing prototypes

--- Oops.js
+++ Oops.js
@@ -136,3 +136,19 @@ Dog.prototype = {
 };
 
 // Remember to set the constructor property when changing the prototype
+function Dog(name) {
+    this.name = name;
+}
+
+Dog.prototype = {
+    constructor: Dog,
+    numLegs: 4,
+    eat: function() {
+      console.log("nom nom nom");
+    },
+    describe: function() {
+      console.log("My name is " + this.name);
+    }
+};
+
+//
